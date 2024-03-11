4e6c7dcbaef7089da1f5822493fcde17aa6d9f80
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-03-11 08:43:59 -0500

    Create OperacionesAritmeticas.py

---
+++ OperacionesAritmeticas.py
@@ -0,0 +1,22 @@
+def OperacionesAritmeticas():
+    a = float(input("Ingrese el primer numero(a)"))
+    b = float(input("Ingrese el primer numero(b)"))
+    
+    #Operacion
+    suma = a + b 
+    resta = a - b 
+    multiplicacion = a + b 
+    
+    #Division entre 0
+    if b ! = 0: ...
+    else: 
+        division = "Division por cero no esta definida"
+    return suma, resta, multiplicacion, division
+
+    #Obtener resultado
+resultado = OperacionesAritmeticas()
+
+print("suma", resultado[0])
+print("resta", resultado[1])
+print("multiplicacion", resultado[2])
+print("division", resultado[3])
